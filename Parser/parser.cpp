75e0572a015d502eb659437b0e85eb14d89dc7c8
lightyears1998 <lightyears1998@hotmail.com>
2019-05-06 01:15:58 +0800

    调整组织

--- Parser/parser.cpp
+++ Parser/parser.cpp
@@ -7,14 +7,54 @@ namespace angelica
 
 	Parser::Parser()
 	{
-		addKeyword("main", SymbolType::KEYWORD_MAIN);
-		addKeyword("int", SymbolType::KEYWORD_INT);
-		addKeyword("char", SymbolType::KEYWORD_CHAR);
-		addKeyword("if", SymbolType::KEYWORD_IF);
-		addKeyword("else", SymbolType::KEYWORD_ELSE);
-		addKeyword("for", SymbolType::KEYWORD_FOR);
-		addKeyword("while", SymbolType::KEYWORD_WHILE);
-		addKeyword("return", SymbolType::KEYWORD_RETURN);
+		// 添加关键词
+		keyword_to_symbol_type_["main"] = SymbolType::KEYWORD_MAIN;
+		keyword_to_symbol_type_["int"] = SymbolType::KEYWORD_INT;
+		keyword_to_symbol_type_["char"] = SymbolType::KEYWORD_CHAR;
+		keyword_to_symbol_type_["if"] = SymbolType::KEYWORD_IF;
+		keyword_to_symbol_type_["else"] = SymbolType::KEYWORD_ELSE;
+		keyword_to_symbol_type_["for"] = SymbolType::KEYWORD_FOR;
+		keyword_to_symbol_type_["while"] = SymbolType::KEYWORD_WHILE;
+		keyword_to_symbol_type_["return"] = SymbolType::KEYWORD_RETURN;
+		keyword_to_symbol_type_["void"] = SymbolType::KEYWORD_VOID;
+
+		// 为单词种别添加助记符
+		symbol_type_to_mnemonics_[SymbolType::KEYWORD_MAIN] = "main";
+		symbol_type_to_mnemonics_[SymbolType::KEYWORD_INT] = "int";
+		symbol_type_to_mnemonics_[SymbolType::KEYWORD_CHAR] = "char";
+		symbol_type_to_mnemonics_[SymbolType::KEYWORD_IF] = "if";
+		symbol_type_to_mnemonics_[SymbolType::KEYWORD_ELSE] = "else";
+		symbol_type_to_mnemonics_[SymbolType::KEYWORD_FOR] = "for";
+		symbol_type_to_mnemonics_[SymbolType::KEYWORD_WHILE] = "while";
+		symbol_type_to_mnemonics_[SymbolType::KEYWORD_RETURN] = "return";
+		symbol_type_to_mnemonics_[SymbolType::KEYWORD_VOID] = "void";
+
+		symbol_type_to_mnemonics_[SymbolType::STRING] = "STRING";
+		symbol_type_to_mnemonics_[SymbolType::ID] = "ID";
+		symbol_type_to_mnemonics_[SymbolType::INT] = "INT";
+		symbol_type_to_mnemonics_[SymbolType::EQUALS] = "=";
+		symbol_type_to_mnemonics_[SymbolType::PLUS] = "+";
+		symbol_type_to_mnemonics_[SymbolType::SUBSTRACT] = "-";
+		symbol_type_to_mnemonics_[SymbolType::MULTIPLE] = "*";
+		symbol_type_to_mnemonics_[SymbolType::DIVIDE] = "/";
+		
+		symbol_type_to_mnemonics_[SymbolType::LS_PARENTHESIS] = "(";
+		symbol_type_to_mnemonics_[SymbolType::RS_PARENTHESIS] = ")";
+		symbol_type_to_mnemonics_[SymbolType::LS_PARENTHESIS] = "[";
+		symbol_type_to_mnemonics_[SymbolType::RS_PARENTHESIS] = "]";
+		symbol_type_to_mnemonics_[SymbolType::LS_PARENTHESIS] = "{";
+		symbol_type_to_mnemonics_[SymbolType::RS_PARENTHESIS] = "}";
+
+		symbol_type_to_mnemonics_[SymbolType::COMMA] = ",";
+		symbol_type_to_mnemonics_[SymbolType::COLON] = ":";
+		symbol_type_to_mnemonics_[SymbolType::SEMICOLON] = ";";
+
+		symbol_type_to_mnemonics_[SymbolType::RELOP_GT] = ">";
+		symbol_type_to_mnemonics_[SymbolType::RELOP_LT] = "<";
+		symbol_type_to_mnemonics_[SymbolType::RELOP_GE] = ">=";
+		symbol_type_to_mnemonics_[SymbolType::RELOP_LE] = "<=";
+		symbol_type_to_mnemonics_[SymbolType::RELOP_EQ] = "==";
+		symbol_type_to_mnemonics_[SymbolType::RELOP_NEQ] = "!=";
 	}
 
 	void Parser::Parse(string source_code)
@@ -27,7 +67,7 @@ namespace angelica
 				continue;
 			}
 
-			if (isalpha(token[0])) {
+			if (isalpha(token[0])) {  // 关键词或标识符
 				size_t cur = idx + 1;  // 待分析的第一个字符的下标
 				while (cur < source_code.length() && (isalpha(source_code[cur]) || isdigit(source_code[cur]))) {
 					token += source_code[cur];
@@ -35,11 +75,18 @@ namespace angelica
 				}
 
 				idx = cur - 1;
-				symbols_.push_back(Symbol(SymbolType::ID, token));  // 还需要考虑是否为关键字
+				if (isKeyword(token)) {  // 关键词
+					SymbolType type = keyword_to_symbol_type_[token];
+					symbols_.push_back(Symbol(type, ""));
+				}
+				else {  // 标识符
+					symbols_.push_back(Symbol(SymbolType::ID, token));
+				}
+				
 				continue;
 			}
 
-			if (isdigit(token[0])) {
+			if (isdigit(token[0])) {  // 整数（不考虑小数）
 				size_t cur = idx + 1;
 				while (cur < source_code.length() && isdigit(source_code[cur])) {
 					token += source_code[cur];
@@ -51,7 +98,7 @@ namespace angelica
 				continue;
 			}
 
-			if (token[0] == '"') {
+			if (token[0] == '"') {  // 字符串
 				token = "";
 				size_t cur = idx + 1;
 				while (cur < source_code.length() && source_code[cur] != '"') {  // 不考虑转义字符
@@ -66,10 +113,25 @@ namespace angelica
 		}
 	}
 
-	bool Parser::IsKeyword(string word)
+	void Parser::defineMnemonicsForSymbolType(SymbolType type, string mnemonics)
+	{
+		symbol_type_to_mnemonics_[type] = mnemonics;
+	}
+
+	void Parser::associateSymbolTypeWithKeyword(SymbolType type, string keyword)
+	{
+		keyword_to_symbol_type_[keyword] = type;
+	}
+
+	bool Parser::isKeyword(string word)
 	{
 		// 若为关键字，则可以在关键字表中找到关键字对应的单词种别。
 		return keyword_to_symbol_type_.find(word) != keyword_to_symbol_type_.end();
+	}
+
+	SymbolType Parser::getKeywordSymbol(string keyword)
+	{
+		return keyword_to_symbol_type_.at(keyword);
 	}
 
 	vector<Symbol>& Parser::GetSymbols()
@@ -82,12 +144,5 @@ namespace angelica
 		string mnemonics = symbol_type_to_mnemonics_[symbol.type];
 		string value = symbol.value != "" ? symbol.value : "null";
 		return string("(") + mnemonics + string(", ") + value + string(")");
-	}
-
-	void Parser::addKeyword(string keyword, SymbolType type)
-	{
-		keyword_to_symbol_type_[keyword] = type;
-		symbol_type_to_keyword_[type] = keyword;
-		symbol_type_to_mnemonics_[type] = keyword;
 	}
 }
--- Parser/parser.h
+++ Parser/parser.h
@@ -17,19 +17,30 @@ namespace angelica
 	public:
 		Parser();
 		
+		// 对指定souce_code进行词法分析
 		void Parse(string source_code);
 
-		bool IsKeyword(string word);
-
+		// 返回保存单词符号的数组
 		vector<Symbol>& GetSymbols();
+
+		// 返回单词符号的字符串表示
 		string GetSymbolString(Symbol symbol);
 
 	private:
-		void addKeyword(string keyword, SymbolType type);
+		// 定义单词符号种别的助记符
+		void defineMnemonicsForSymbolType(SymbolType type, string mnemonics);
+		
+		// 关联单词符号种别与关键词
+		void associateSymbolTypeWithKeyword(SymbolType type, string keyword);
 
-		vector<Symbol> symbols_;
-		map<string, SymbolType> keyword_to_symbol_type_;
-		map<SymbolType, string> symbol_type_to_keyword_;
-		map<SymbolType, string> symbol_type_to_mnemonics_;
+		// 判断word是否为关键词
+		bool isKeyword(string word);
+
+		// 返回关键词对应的单词符号种别
+		SymbolType getKeywordSymbol(string keyword);
+
+		vector<Symbol> symbols_;  // 用以保存单词符号的数组
+		map<string, SymbolType> keyword_to_symbol_type_;  // 保存关键词对应的单词符号种别
+		map<SymbolType, string> symbol_type_to_mnemonics_;  // 保存单词符号种别对应的助记符
 	};
 }
