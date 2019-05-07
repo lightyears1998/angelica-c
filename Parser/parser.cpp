a8d48ee53f514935fd31ffcfb965378b2160833c
lightyears1998 <lightyears1998@hotmail.com>
2019-05-07 21:22:21 +0800

    完成词法分析基本功能

--- Parser/parser.cpp
+++ Parser/parser.cpp
@@ -59,27 +59,132 @@ namespace angelica
 
 	void Parser::Parse(string source_code)
 	{
+		symbols_.clear();  // 在词法分析开始前，清空保存单词符号的容器
+
 		for (size_t idx = 0; idx < source_code.length(); ++idx)
 		{
-			string token(1, source_code[idx]);  // 将token初始化为input[idx]
+			string token;  // 用于保存单词符号的字符串
+			token = source_code[idx];  // 将token初始化为input[idx]
 
 			if (isspace(token[0])) {  // 滤除空白字符
 				continue;
 			}
 
-			if (isalpha(token[0])) {  // 关键词或标识符
+			if (token[0] == '=') { // 识别为“=”或“==”
+				if (idx + 1 < source_code.length() && source_code.substr(idx, 2) == "==")
+				{  // “==”
+					symbols_.push_back(Symbol(SymbolType::RELOP_EQ, ""));
+				}
+				else
+				{  // “=”
+					symbols_.push_back(Symbol(SymbolType::ASSIGNMENT, ""));
+				}
+
+				continue;
+			}
+
+			if (token[0] == '+') {  // “+”
+				symbols_.push_back(Symbol(SymbolType::PLUS, ""));
+				continue;
+			}
+
+			if (token[0] == '-') {  // “-”
+				symbols_.push_back(Symbol(SymbolType::SUBSTRACT, ""));
+				continue;
+			}
+
+			if (token[0] == '*') {  // “*”
+				symbols_.push_back(Symbol(SymbolType::MULTIPLE, ""));
+				continue;
+			}
+
+			if (token[0] == '/') {  // “//”、“/*”或“/”
+				if (idx + 1 < source_code.length())
+				{
+					if (source_code[idx + 1] == '/')
+					{  // “//”
+						++idx;
+						while (idx < source_code.length() && source_code[idx] != '\n') 
+							++idx;  // 滤除注释
+
+						continue;
+					}
+					else
+					{  // “/*”
+						++idx;
+						while (idx + 1 < source_code.length() && source_code.substr(idx, 2) != "*/")
+							++idx;  // 滤除注释
+						
+						continue;
+					}
+				}
+				else
+				{  // “/”
+					symbols_.push_back(Symbol(SymbolType::DIVIDE, ""));
+
+					continue;
+				}
+			}
+
+			if (token[0] == '(') {  // “(”
+				symbols_.push_back(Symbol(SymbolType::LS_PARENTHESIS, ""));
+				continue;
+			}
+
+			if (token[0] == ')') {  // “)”
+				symbols_.push_back(Symbol(SymbolType::RS_PARENTHESIS, ""));
+				continue;
+			}
+
+			if (token[0] == '[') {  // “[”
+				symbols_.push_back(Symbol(SymbolType::LM_PARENTHESIS, ""));
+				continue;
+			}
+
+			if (token[0] == ']') {  // “]”
+				symbols_.push_back(Symbol(SymbolType::RM_PARENTHESIS, ""));
+				continue;
+			}
+
+			if (token[0] == '{') {  // “{”
+				symbols_.push_back(Symbol(SymbolType::LL_PARENTHESIS, ""));
+				continue;
+			}
+
+			if (token[0] == '}') {  // “}”
+				symbols_.push_back(Symbol(SymbolType::RL_PARENTHESIS, ""));
+				continue;
+			}
+
+			if (token[0] == ',') {  // “,”
+				symbols_.push_back(Symbol(SymbolType::COMMA, ""));
+				continue;
+			}
+
+			if (token[0] == ':') {  // “:”
+				symbols_.push_back(Symbol(SymbolType::COLON, ""));
+				continue;
+			}
+
+			if (token[0] == ';') {  // “;”
+				symbols_.push_back(Symbol(SymbolType::SEMICOLON, ""));
+				continue;
+			}
+
+			if (isalpha(token[0]) || token[0] == '_') {  // 识别为关键词或标识符
 				size_t cur = idx + 1;  // 待分析的第一个字符的下标
-				while (cur < source_code.length() && (isalpha(source_code[cur]) || isdigit(source_code[cur]))) {
+				while (cur < source_code.length() && (isalpha(source_code[cur]) || isdigit(source_code[cur]) || source_code[cur] == '_')) 
+				{
 					token += source_code[cur];
 					cur += 1;
 				}
 
-				idx = cur - 1;
-				if (isKeyword(token)) {  // 关键词
+				idx = cur - 1;  // 回退一个字符
+				if (isKeyword(token)) {  // 识别为关键词
 					SymbolType type = keyword_to_symbol_type_[token];
 					symbols_.push_back(Symbol(type, ""));
 				}
-				else {  // 标识符
+				else {  // 识别为标识符
 					symbols_.push_back(Symbol(SymbolType::ID, token));
 				}
 				
@@ -110,6 +215,55 @@ namespace angelica
 				symbols_.push_back(Symbol(SymbolType::STRING, token));
 				continue;
 			}
+
+			if (token[0] == '>')  // “>”、“>=”
+			{
+				if (idx + 1 < source_code.length() && source_code[idx + 1] == '=')
+				{
+					symbols_.push_back(Symbol(SymbolType::RELOP_GE, ""));
+					++idx;
+					continue;
+				}
+
+				symbols_.push_back(Symbol(SymbolType::RELOP_GT, ""));
+				continue;
+			}
+
+			if (token[0] == '<')  // “<”、“<=”
+			{
+				if (idx + 1 < source_code.length() && source_code[idx + 1] == '=')
+				{
+					symbols_.push_back(Symbol(SymbolType::RELOP_LE, ""));
+					++idx;
+					continue;
+				}
+
+				symbols_.push_back(Symbol(SymbolType::RELOP_LT, ""));
+				continue;
+			}
+
+			if (token[0] == '=')  // “=”、“==”
+			{
+				if (idx + 1 < source_code.length() && source_code[idx + 1] == '=')
+				{
+					symbols_.push_back(Symbol(SymbolType::RELOP_EQ, ""));
+					++idx;
+					continue;
+				}
+				
+				symbols_.push_back(Symbol(SymbolType::ASSIGNMENT, ""));
+				continue;
+			}
+
+			if (token[0] == '!' && idx + 1 < source_code.length() && source_code[idx+1] == '=')  // “!=”
+			{  // 识别为“!=”
+				symbols_.push_back(Symbol(SymbolType::RELOP_NEQ, ""));
+				++idx;
+				continue;
+			}
+
+			// 如果没有被任何一个if语句捕捉，则应该报告错误
+			throw new exception("输入内容不合词法。");
 		}
 	}
 
