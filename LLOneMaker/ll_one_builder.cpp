a79c0df9749aa62624e137afc355fe80d9981a8c
lightyears <lightyears1998@hotmail.com>
2019-06-20 14:50:06 +0800

    将权责分散到函数中

--- LLOneMaker/ll_one_builder.cpp
+++ LLOneMaker/ll_one_builder.cpp
@@ -4,23 +4,29 @@
 
 #include "ll_one_builder.h"
 
-#include <set>
-#include <functional>
-
 namespace angelica {
 	using namespace std;
 
 	LLOneAnalyzer LLOneBuilder::BuildAnalyzer()
+	{ 
+		getInputFromParser();     // 读取词法分析器的输入
+		diminishLeftRecursion();  // 消除左递归
+		removeLookBack();         // 消除回溯
+		
+		printNonterminalSymbolSet();  // 打印非终结符集合
+		printFirstSet();              // 打印First集合
+		printFollowSet();             // 打印Follow集
+
+		buildAnalyzer();  // 生成文法对应的分析器
+		return analyzer;
+	}
+
+	void LLOneBuilder::getInputFromParser()
 	{
-		LLOneAnalyzer analyzer;
-
-		set<Symbol> nonterminal_set;                // 保存所有非终结符的集合
-		map<Symbol, vector<Symbol>> left_to_right;  // 左部非终结符到右部符号的映射
-
 		vector<Symbol> line = parser_.ParseLineFromInputStream(); // 从Parser中获取第一行输入
-													
+
 		// 将第一条产生式的左部定义为文法的开始符号
-		Symbol start_symbol(line[0]);
+		start_symbol = line[0];
 		analyzer.DefineStartSymbol(start_symbol);
 
 		for (line; line.size() != 0; line = parser_.ParseLineFromInputStream()) {
@@ -34,22 +40,22 @@ namespace angelica {
 			// 程序仅考虑形如 A→Aα|β 和 A→α 两种形式的产生式
 			if (line[2] == line[0]) {  // A→Aα|β 需要消除左递归的情况
 				auto delim = find(right.begin(), right.end(), Symbol(SymbolType::STRUCTURE, "|"));  // 竖线的位置
-				
+
 				// 产生式形如 A→Aα|β， 需要将 A→Aα|β 改写为 A→βA', A'→αA'|ε
 
 				// α和β
-				vector<Symbol> alpha(right.begin() + 1, delim); 
+				vector<Symbol> alpha(right.begin() + 1, delim);
 				vector<Symbol> beta(delim + 1, right.end());
 
 				Symbol adapted{ SymbolType::NONTERMINAL, left.value + "'" };
 				nonterminal_set.insert(adapted);
 
 				// βA'
-				vector<Symbol> beta_adapted(beta); 
+				vector<Symbol> beta_adapted(beta);
 				beta_adapted.push_back(adapted);
-				
+
 				// αA'|ε
-				vector<Symbol> alpha_adapted_epsilon(alpha); 
+				vector<Symbol> alpha_adapted_epsilon(alpha);
 				alpha_adapted_epsilon.push_back(adapted);
 				alpha_adapted_epsilon.push_back(Symbol(SymbolType::STRUCTURE, "|"));
 				alpha_adapted_epsilon.push_back(SYMBOL_NULL);
@@ -63,8 +69,13 @@ namespace angelica {
 			}
 		}
 		cout << endl;
+	}
 
+	void LLOneBuilder::diminishLeftRecursion()
+	{
 		cout << "消除左递归后的文法：" << endl;
+
+		// 算法的局限性：只能消除直接左递归，只能处理 A→Aα|β 和 A→α 两种形式的产生式
 		for (auto it = left_to_right.begin(); it != left_to_right.end(); ++it) {
 			cout << it->first;
 			cout << "→";
@@ -74,41 +85,25 @@ namespace angelica {
 			cout << endl;
 		}
 		cout << endl;
+	}
 
-		// 程序不考虑回溯
+	void LLOneBuilder::removeLookBack()
+	{
+		// 程序不考虑回溯，因此这部分留空
+	}
 
+	void LLOneBuilder::printNonterminalSymbolSet()
+	{
 		cout << "文法的非终结符集合：";
 		for (auto it = nonterminal_set.begin(); it != nonterminal_set.end(); ++it) {
 			cout << it->value << " ";
 		}
 		cout << endl;
 		cout << "其中，文法的开始符号为：" << start_symbol << endl << endl;
-		
-		// 获取一个终结符所可能推导出的第一个非终结符的集合（First集）
-		function<set<Symbol>(Symbol)> getPossibleFirstTerminalSymbol = [&](Symbol left) {
+	}
 
-			set<Symbol> possible_terminal;
-			
-			auto right = left_to_right[left];
-			for (size_t it = 0; it < right.size(); ++it) {
-				Symbol current = right[it];
-				if (current.type == SymbolType::TERMINAL) {
-					possible_terminal.insert(current);
-
-					while (it < right.size() && right[it].value != "|") ++it;  // 跳至下一个“|”之后
-				}
-				else if (current.type == SymbolType::NONTERMINAL) {
-					set<Symbol> inner_possible_terminal = getPossibleFirstTerminalSymbol(current);
-					possible_terminal.insert(inner_possible_terminal.begin(), inner_possible_terminal.end());
-						
-					if (inner_possible_terminal.count(SYMBOL_NULL) == 0) {
-						while (it < right.size() && right[it].value != "|") ++it;  // 跳至下一个“|”之后
-					}
-				}
-			}
-			return possible_terminal;
-		};
-
+	void LLOneBuilder::printFirstSet()
+	{
 		cout << "构造的First集合" << endl;
 		for (Symbol left : nonterminal_set) {
 			cout << "First(" << left << ")\t= { ";
@@ -119,64 +114,26 @@ namespace angelica {
 		}
 		cout << endl;
 
-		// 获取一个终结符之后所可能推导出的第一个非终结符的集合（Next集）
-		function<set<Symbol>(Symbol)> getPossibleNextTerminalSymbol = [&](Symbol left) {
-			set<Symbol> possible_terminal;
 
-			// 遍历所有的产生式
-			for (auto it = nonterminal_set.begin(); it != nonterminal_set.end(); ++it) {
-				Symbol current_left = *it;
-				auto current_right = left_to_right[current_left];
+	}
 
-				// 遍历一条复杂产生式中所有的字符
-				bool continueInNeed = false;  // 在一些情况下需要忽略循环中current_right[it] == left的判定而继续
-				for (size_t it = 0; it < current_right.size(); ++it) {
-					if (current_right[it] == left || continueInNeed) {
-						continueInNeed = false;  // 重置标记
-
-						if (it + 1 == current_right.size() || current_right[it + 1] == Symbol(SymbolType::STRUCTURE, "|")) { // 右边的一个字符是界
-							if (current_left != left) {
-								auto next_set_of_current_left = getPossibleNextTerminalSymbol(current_left);
-								possible_terminal.insert(next_set_of_current_left.begin(), next_set_of_current_left.end());
-							}
-						}
-						else {  // 右边字符是终结符或非终结符
-							Symbol next_symbol = current_right[it + 1];
-
-							if (next_symbol.type == SymbolType::TERMINAL) {
-								possible_terminal.insert(next_symbol);
-							}
-							else {  // next_symbol.type == SymbolType::NONTERMINAL
-								auto first_set_of_next_symbol = getPossibleFirstTerminalSymbol(next_symbol);
-								if (first_set_of_next_symbol.count(SYMBOL_NULL) != 0) {
-									continueInNeed = true;
-									first_set_of_next_symbol.erase(SYMBOL_NULL);
-								}
-								possible_terminal.insert(first_set_of_next_symbol.begin(), first_set_of_next_symbol.end());
-							}
-						}
-					}
-				}
-			}
-
-			if (left == start_symbol) {
-				possible_terminal.insert(SYMBOL_END);
-			}
-			return possible_terminal;
-		};
-
-		cout << "构造的Next集合：" << endl;
+	void LLOneBuilder::printFollowSet()
+	{
+		cout << "构造的Follow集合：" << endl;
 		for (Symbol sym : nonterminal_set) {
-			cout << "Next (" << sym << ")\t= { ";
-			
-			auto next_set = getPossibleNextTerminalSymbol(sym);
+			cout << "Follow(" << sym << ")\t= { ";
+
+			auto next_set = getPossibleFollowTerminalSymbol(sym);
 			for (Symbol sym : next_set) {
 				cout << sym << " ";
 			}
 			cout << "}" << endl;
 		}
 		cout << endl;
+	}
 
+	void LLOneBuilder::buildAnalyzer()
+	{
 		// 根据产生式产生分析表
 		for (auto it = nonterminal_set.begin(); it != nonterminal_set.end(); ++it) {
 			Symbol left = *it;
@@ -201,7 +158,7 @@ namespace angelica {
 
 						if (inner_possible_terminal.count(SYMBOL_NULL) == 0) break;
 					}
-					
+
 				}
 				else if (current_right[0].type == SymbolType::TERMINAL && current_right[0] != SYMBOL_NULL) {
 					// 若右部的第一个符号为终结符，则该产生式只可能推导出这个终结符
@@ -210,7 +167,7 @@ namespace angelica {
 				else {  // current_right[0] == SYMBOL_NULL
 					// 若右部是一个空终结符，则需要考虑其他产生式才能得到该产生式所可能推导出的终结符
 					// 这个分支本质上是求Follow集
-					possible_terminal = getPossibleNextTerminalSymbol(left);
+					possible_terminal = getPossibleFollowTerminalSymbol(left);
 				}
 
 				// 对此产生式，在分析表中进行标记
@@ -219,10 +176,78 @@ namespace angelica {
 				}
 			}
 		}
-		
+
 		// 打印产生的分析表
 		analyzer.PrintGrammarRulesTable();
+	}
 
-		return analyzer;
+	set<Symbol> LLOneBuilder::getPossibleFirstTerminalSymbol(Symbol left)
+	{
+		set<Symbol> possible_terminal;
+
+		auto right = left_to_right[left];
+		for (size_t it = 0; it < right.size(); ++it) {
+			Symbol current = right[it];
+			if (current.type == SymbolType::TERMINAL) {
+				possible_terminal.insert(current);
+
+				while (it < right.size() && right[it].value != "|") ++it;  // 跳至下一个“|”之后
+			}
+			else if (current.type == SymbolType::NONTERMINAL) {
+				set<Symbol> inner_possible_terminal = getPossibleFirstTerminalSymbol(current);
+				possible_terminal.insert(inner_possible_terminal.begin(), inner_possible_terminal.end());
+
+				if (inner_possible_terminal.count(SYMBOL_NULL) == 0) {
+					while (it < right.size() && right[it].value != "|") ++it;  // 跳至下一个“|”之后
+				}
+			}
+		}
+		return possible_terminal;
+	}
+
+	set<Symbol> LLOneBuilder::getPossibleFollowTerminalSymbol(Symbol left)
+	{
+		set<Symbol> possible_terminal;
+
+		// 遍历所有的产生式
+		for (auto it = nonterminal_set.begin(); it != nonterminal_set.end(); ++it) {
+			Symbol current_left = *it;
+			auto current_right = left_to_right[current_left];
+
+			// 遍历一条复杂产生式中所有的字符
+			bool continueInNeed = false;  // 在一些情况下需要忽略循环中current_right[it] == left的判定而继续
+			for (size_t it = 0; it < current_right.size(); ++it) {
+				if (current_right[it] == left || continueInNeed) {
+					continueInNeed = false;  // 重置标记
+
+					if (it + 1 == current_right.size() || current_right[it + 1] == Symbol(SymbolType::STRUCTURE, "|")) { // 右边的一个字符是界
+						if (current_left != left) {
+							auto next_set_of_current_left = getPossibleFollowTerminalSymbol(current_left);
+							possible_terminal.insert(next_set_of_current_left.begin(), next_set_of_current_left.end());
+						}
+					}
+					else {  // 右边字符是终结符或非终结符
+						Symbol next_symbol = current_right[it + 1];
+
+						if (next_symbol.type == SymbolType::TERMINAL) {
+							possible_terminal.insert(next_symbol);
+						}
+						else {  // next_symbol.type == SymbolType::NONTERMINAL
+							auto first_set_of_next_symbol = getPossibleFirstTerminalSymbol(next_symbol);
+							if (first_set_of_next_symbol.count(SYMBOL_NULL) != 0) {
+								continueInNeed = true;
+								first_set_of_next_symbol.erase(SYMBOL_NULL);
+							}
+							possible_terminal.insert(first_set_of_next_symbol.begin(), first_set_of_next_symbol.end());
+						}
+					}
+				}
+			}
+		}
+
+		if (left == start_symbol) {
+			possible_terminal.insert(SYMBOL_END);
+		}
+		return possible_terminal;
 	}
 }
--- LLOneMaker/ll_one_builder.h
+++ LLOneMaker/ll_one_builder.h
@@ -8,18 +8,55 @@
 #include "parser.h"
 #include "ll_one_analyzer.h"
 
+#include <set>
+#include <map>
+#include <functional>
+
 namespace angelica {
 	// LL(1)分析表生成器
 	// 能自动生成符合LL(1)规则的分析器。
 	class LLOneBuilder
 	{
 	private:
-		Parser parser_;
+		Parser parser_;                             // 输入文法使用的词法分析器
+
+		LLOneAnalyzer analyzer;                     // 与输入文法对应的分析器
+		set<Symbol> nonterminal_set;                // 保存所有非终结符的集合
+		Symbol start_symbol;                        // 保存文法的开始符号
+		map<Symbol, vector<Symbol>> left_to_right;  // 左部非终结符到右部符号的映射
 
 	public:
 		LLOneBuilder(Parser &parser) : parser_(parser) {};
 
 		// 从Parser中获取表示LL(1)文法的符号，并返回对应文法的LL(1)分析器。
 		LLOneAnalyzer BuildAnalyzer();
+
+	private:
+		// 从Parser中获取文法输入
+		void getInputFromParser();
+		
+		// 消除左递归
+		void diminishLeftRecursion();
+
+		// 消除回溯
+		void removeLookBack();
+
+		// 获取一个终结符所可能推导出的第一个非终结符的集合（First集）
+		set<Symbol> getPossibleFirstTerminalSymbol(Symbol left);
+
+		// 获取一个终结符之后所可能推导出的第一个非终结符的集合（Follow集）
+		set<Symbol> getPossibleFollowTerminalSymbol(Symbol left);
+
+		// 打印非终结符集合
+		void printNonterminalSymbolSet();
+	
+		// 打印First集
+		void printFirstSet();
+
+		// 打印Follow集
+		void printFollowSet();
+
+		// 生成文法对应的分析器
+		void buildAnalyzer();
 	};
 }
